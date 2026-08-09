be2fe0a9ded5daa93fff9e9945df9262c6149c63
monisharajendrann-crypto <monisharajendrann@gmail.com>
2026-08-09 22:03:01 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -50,7 +50,7 @@ if ingredients_list:
 
 smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon") 
 # st.text(smoothiefroot_response)
-sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True_
+sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
 
 
 # if ingredients_string:

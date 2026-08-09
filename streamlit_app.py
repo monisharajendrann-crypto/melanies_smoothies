8cdfbfdd890986a304c9d990c04518c218c0c04d
monisharajendrann-crypto <monisharajendrann@gmail.com>
2026-08-09 22:34:49 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -66,7 +66,7 @@ if ingredients_list:
         session.sql(my_insert_stmt).collect()
         st.success('Your Smoothie is ordered!', icon="✅")
 
-smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon") 
+smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/{search_on}") 
 # st.text(smoothiefroot_response)
 sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
 

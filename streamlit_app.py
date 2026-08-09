76392059ff6713e27394f88a3a63086097f58176
monisharajendrann-crypto <monisharajendrann@gmail.com>
2026-08-09 22:32:42 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -29,8 +29,8 @@ my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT
 # st.stop
 
 pd_df=my_dataframe.to_pandas()
-st.dataframe(pd_df)
-st.stop
+# st.dataframe(pd_df)
+# st.stop
 
 ingredients_list = st.multiselect(
     'Choose up yo 5 ingredients : '
@@ -45,7 +45,8 @@ if ingredients_list:
     ingredients_string = ''
     for fruit_chosen in ingredients_list:
         ingredients_string += fruit_chosen + ' '
-
+        search_on=pd_df.loc[pd_df['FRUIT_NAME'] == fruit_chosen, 'SEARCH_ON'].iloc[0]
+        st.write('The search value for ', fruit_chosen,' is ', search_on, '.')
         st.subheader(fruit_chosen + ' Nutrition Information')
         fruityvice_response = requests.get("https://my.smoothiefroot.com/api/fruit/" + fruit_chosen.lower())
         if fruityvice_response.status_code == 200:
